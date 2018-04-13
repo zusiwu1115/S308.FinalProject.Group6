496134099b16ab051872e04bd67264c5330e71e2
zusiwu1115 <zusiwu@indiana.edu>
2018-04-13 17:49:00 -0400

    Member sign up validation

--- S308.FinalProject.Group6/FitnessClub/MembershipSignup.xaml.cs
+++ S308.FinalProject.Group6/FitnessClub/MembershipSignup.xaml.cs
@@ -23,5 +23,41 @@ namespace FitnessClub
         {
             InitializeComponent();
         }
+
+        private void btnQuote_Click(object sender, RoutedEventArgs e)
+        {
+            //Declear varibales
+            string strFirstName;
+            string strLastName;
+            string strCardType;
+            string strCreditCardNum;
+            string strPhoneNum;
+            string strEmail;
+            string strGender;
+            double dblAge;
+            double dblWeight;
+
+            //Validation of input
+            if (!txtEmail.Text.Contains("@"))
+            { MessageBox.Show("Please enter a valid email address");
+                return;}
+            Double.TryParse(txtAge.Text, out dblAge);
+            { MessageBox.Show("Please enter a positive number of age");
+                return; }
+            Double.TryParse(txtWeight.Text, out dblWeight);
+              { MessageBox.Show("Please enter a positive number of weight");
+                return; }
+            if (dblAge <= 0)
+            { MessageBox.Show("Please enter a positive number of age");
+                return;
+            }
+            if (dblWeight <= 0)
+            { MessageBox.Show("Please enter a positive number of weight");
+                return; }
+
+
+
+
+        }
     }
 }
