330d9c0c69d74e1214fccbe6c71fefcd82823cb2
jennie99908 <chen417@iu.edu>
2018-04-26 23:14:16 -0400

    new

--- S308.FinalProject.Group6/FitnessClub/PricingManagement.xaml.cs
+++ S308.FinalProject.Group6/FitnessClub/PricingManagement.xaml.cs
@@ -11,8 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using Newtonsoft.Json;
-using System.IO;
 
 namespace FitnessClub
 {
@@ -25,32 +23,5 @@ namespace FitnessClub
         {
             InitializeComponent();
         }
-
-        private void btnMainMenu_Click(object sender, RoutedEventArgs e)
-        {
-            MainMenu MainMenuWindow = new MainMenu();
-            MainMenuWindow.Show();
-            this.Close();
-        }
-
-        //Declear Variable
-        string strPricingType;
-
-        //convert selected item in the input to string
-        ComboBoxItem cbiMembershipType = (ComboBoxItem)cobMembershipType.SelectedItem;
-        string strMembershipType = cbiMembershipType.Content.ToString();
-
-        //validate inputs of Membership type, startdate and Additional Features
-            if (cobMembershipType.SelectedIndex=0)
-            { MessageBox.Show("Please select a Membership Type from the dropdown List.");
-            return; }
-
-
-
-        private void btnSave_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
     }
 }
